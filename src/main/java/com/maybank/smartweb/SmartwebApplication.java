0d7a9f6ce5e41166debd8c35247bc283d5c4c9ca
rzlwbo1 <rizalwb2@gmail.com>
2023-01-09 13:50:34 +0700

    hello world

---
+++ src/main/java/com/maybank/smartweb/SmartwebApplication.java
@@ -0,0 +1,23 @@
+package com.maybank.smartweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+@Controller
+public class SmartwebApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	public String index() {
+		return "<h2><b>Spring Hello World</b></h2>";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maybank/smartweb/SmartwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maybank.smartweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
