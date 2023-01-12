4817c55ed8620e1eef10ed4f314e98ddd1e7cac0
rzlwbo1 <rizalwb2@gmail.com>
2023-01-12 15:14:01 +0700

    autentikasi dan restrict pages done

--- src/main/java/com/maybank/smartweb/config/MyAuth.java
+++ src/main/java/com/maybank/smartweb/config/MyAuth.java
@@ -5,11 +5,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -31,6 +33,15 @@ public class MyAuth {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/employee").hasAnyAuthority("Regular");
+        httpSecurity.authorizeRequests().antMatchers("/car").hasAnyAuthority("Admin");
+        httpSecurity.authorizeRequests().anyRequest().authenticated();
+        httpSecurity.authorizeRequests().and().formLogin();
+
+        return httpSecurity.build();
+    }
 
     // ini digunakan saat deploy dan gabisa nambah user
 
