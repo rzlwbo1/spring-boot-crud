71e5783c74b847a3c0b6c4103dd2fa3d74d14a25
rzlwbo1 <rizalwb2@gmail.com>
2023-01-13 13:34:08 +0700

    regis lgi optional to list {role}

--- src/main/java/com/maybank/smartweb/controller/HomeController.java
+++ src/main/java/com/maybank/smartweb/controller/HomeController.java
@@ -1,21 +1,30 @@
 package com.maybank.smartweb.controller;
 
-import com.maybank.smartweb.entity.Car;
-import com.maybank.smartweb.entity.Employee;
-import com.maybank.smartweb.entity.User;
-import com.maybank.smartweb.repository.EmployeeRepo;
+import com.maybank.smartweb.entity.*;
+import com.maybank.smartweb.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
+    @Autowired
+    private UserRepo userRepo;
+    @Autowired
+    private RoleRepo roleRepo;
 
     @GetMapping
     public String index(Model model) {
@@ -32,12 +41,39 @@ public class HomeController {
     }
 
     @PostMapping("/regis/save")
-    public String saveUser(Model model) {
+    public String saveUser(@Valid @ModelAttribute("userForm") User userForm,
+                           BindingResult result,
+                           Model model) {
 
 
 
-        model.addAttribute("userForm", new User());
-        return "register";
+        // create user dan buat list role
+//        List<Role> roleList = new ArrayList<>();
+//        roleList.add(roleAdmin);
+//        roleList.add(roleReguler);
+//
+//        List<Role> userRoleList = new ArrayList<>();
+//        userRoleList.add(roleReguler);
+
+
+
+        if (userForm.getRoles().equals("Reguler") ) {
+
+            List<Role> roles = this.roleRepo.findById(Long.valueOf(1)).stream().toList();
+
+            User user = new User();
+            user.setUsername(userForm.getUsername());
+            user.setEmail(userForm.getEmail());
+            user.setPassword(new BCryptPasswordEncoder().encode(userForm.getPassword()));
+            user.setRoles(roles);
+
+            System.out.println(user);
+        }
+
+
+//        this.userRepo.save(user);
+
+        return "redirect:/register";
     }
 
 
