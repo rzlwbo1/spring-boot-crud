0900a34ceb112ac9af19c1b712ca4dacbaa49be7
rzlwbo1 <rizalwb2@gmail.com>
2023-01-13 11:23:42 +0700

    ui regis

--- src/main/java/com/maybank/smartweb/controller/HomeController.java
+++ src/main/java/com/maybank/smartweb/controller/HomeController.java
@@ -2,11 +2,13 @@ package com.maybank.smartweb.controller;
 
 import com.maybank.smartweb.entity.Car;
 import com.maybank.smartweb.entity.Employee;
+import com.maybank.smartweb.entity.User;
 import com.maybank.smartweb.repository.EmployeeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -21,6 +23,21 @@ public class HomeController {
         String springMessage = "Hello view ini dari model";
         model.addAttribute("springMessage", springMessage);
         return "Home";
+    }
+
+    @GetMapping("/regis")
+    public String regis(Model model) {
+        model.addAttribute("userForm", new User());
+        return "register";
+    }
+
+    @PostMapping("/regis/save")
+    public String saveUser(Model model) {
+
+
+
+        model.addAttribute("userForm", new User());
+        return "register";
     }
 
 
