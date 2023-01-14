7148ecedb3bc1dcea153d4e3f4967a8cf5a1c17b
rzlwbo1 <rizalwb2@gmail.com>
2023-01-14 20:44:51 +0700

    soal logic

--- src/main/java/com/maybank/smartweb/extras/soal5.java
+++ src/main/java/com/maybank/smartweb/extras/soal5.java
@@ -1,14 +1,16 @@
 package com.maybank.smartweb.extras;
 
-public class soal4 {
+public class soal5 {
+
     public static void main(String[] args) {
+
         int n = 9;
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                if ( i == j || i + j == n - 1 || i == (n+1)/2-1 || j == (n+1)/2-1 ) {
-                    System.out.print(" *");
-                }else {
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i == j || j == n - i - 1) {
+                    System.out.print("* ");
+                } else {
                     System.out.print("  ");
                 }
             }
